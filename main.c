d90f46da2314c9deae7d92a6304274f643feb963
Tom <quistions@buildcomics.com>
2021-02-18 19:48:19 +0100

    first setup

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    while (true) {
+        printf("Hello, test!\n");
+        sleep_ms(1000);
+    }
+    return 0;
+}
