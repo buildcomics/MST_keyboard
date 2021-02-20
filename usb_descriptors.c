a6c6a91c34ecf73d5f3be7bb17748de36af30d04
Tom <quistions@buildcomics.com>
2021-02-20 18:18:25 +0100

    Working with full HID report

--- usb_descriptors.c
+++ usb_descriptors.c
@@ -76,7 +76,45 @@ uint8_t const *tud_descriptor_device_cb(void) {
 // Ask help in the discussion for telephony setup?https://github.com/hathach/tinyusb/discussions
 uint8_t const desc_hid_report[] =
         {
-                TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(REPORT_ID_KEYBOARD))
+            HID_USAGE_PAGE ( HID_USAGE_PAGE_DESKTOP     )                    ,\
+            HID_USAGE      ( HID_USAGE_DESKTOP_KEYBOARD )                    ,\
+            HID_COLLECTION ( HID_COLLECTION_APPLICATION )                    ,\
+            /* Report ID if any */\
+            HID_REPORT_ID(REPORT_ID_KEYBOARD) \
+            /* 8 bits Modifier Keys (Shfit, Control, Alt) */ \
+            HID_USAGE_PAGE ( HID_USAGE_PAGE_KEYBOARD )                     ,\
+            HID_USAGE_MIN    ( 224                                    )  ,\
+            HID_USAGE_MAX    ( 231                                    )  ,\
+            HID_LOGICAL_MIN  ( 0                                      )  ,\
+            HID_LOGICAL_MAX  ( 1                                      )  ,\
+            HID_REPORT_COUNT ( 8                                      )  ,\
+            HID_REPORT_SIZE  ( 1                                      )  ,\
+            HID_INPUT        ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE )  ,\
+            /* 8 bit reserved */ \
+            HID_REPORT_COUNT ( 1                                      )  ,\
+            HID_REPORT_SIZE  ( 8                                      )  ,\
+            HID_INPUT        ( HID_CONSTANT                           )  ,\
+            /* 6-byte Keycodes */ \
+            HID_USAGE_PAGE ( HID_USAGE_PAGE_KEYBOARD )                     ,\
+            HID_USAGE_MIN    ( 0                                   )     ,\
+            HID_USAGE_MAX    ( 255                                 )     ,\
+            HID_LOGICAL_MIN  ( 0                                   )     ,\
+            HID_LOGICAL_MAX  ( 255                                 )     ,\
+            HID_REPORT_COUNT ( 6                                   )     ,\
+            HID_REPORT_SIZE  ( 8                                   )     ,\
+            HID_INPUT        ( HID_DATA | HID_ARRAY | HID_ABSOLUTE )     ,\
+            /* 5-bit LED Indicator Kana | Compose | ScrollLock | CapsLock | NumLock */ \
+            HID_USAGE_PAGE  ( HID_USAGE_PAGE_LED                   )       ,\
+            HID_USAGE_MIN    ( 1                                       ) ,\
+            HID_USAGE_MAX    ( 5                                       ) ,\
+            HID_REPORT_COUNT ( 5                                       ) ,\
+            HID_REPORT_SIZE  ( 1                                       ) ,\
+            HID_OUTPUT       ( HID_DATA | HID_VARIABLE | HID_ABSOLUTE  ) ,\
+            /* led padding */ \
+            HID_REPORT_COUNT ( 1                                       ) ,\
+            HID_REPORT_SIZE  ( 3                                       ) ,\
+            HID_OUTPUT       ( HID_CONSTANT                            ) ,\
+            HID_COLLECTION_END \
         };
 
 // Invoked when received GET HID REPORT DESCRIPTOR
